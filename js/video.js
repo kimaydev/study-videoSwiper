fa3c66dde16dfdb7763bd30fed48fa6c4291b92c
kimaydev <kimaydev@gmail.com>
2023-05-18 23:46:36 +0900

    feat: 슬라이드 레이아웃 기본 퍼블리싱 작업 및 swiper.js 반영 완료

--- js/video.js
+++ js/video.js
@@ -1,3 +1,27 @@
 window.onload = function(){
-    
+    let swVideo = new Swiper(".sw-video", {
+        loop: true,
+        pagination: {
+            el: ".sw-video-pagination",
+            type: "fraction",
+            formatFractionCurrent: function (number) {
+                if (number < 10){
+                    return '0' + number;
+                } else{
+                    return number;
+                }
+            },
+            formatFractionTotal: function (number){                
+                if (number < 10){
+                    return '0' + number;
+                } else{
+                    return number;
+                }
+            }
+        },
+        navigation: {
+            nextEl: ".sw-video-next",
+            prevEl: ".sw-video-prev",
+        },
+    });
 };
